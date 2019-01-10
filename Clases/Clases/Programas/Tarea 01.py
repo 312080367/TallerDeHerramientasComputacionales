16c74aa623ccf5f9a127ec878580d5b6b8d403c2
312080367 <asaped96@gmail.com>
2019-01-10 17:00:46 -0600

    Tarea 01: Problema de velocidad en Python.

---
+++ Clases/Clases/Programas/Tarea 01.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Un automóvil mantiene una aceleración constante de 8 m/s². Si su velocidad inicial era de 20 m/s al norte, ¿cuál será su velocidad después de 6s?
+ac= 8
+v0= 20
+t=6
+vf= v0 + ac*t
+print vf
