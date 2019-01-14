123db041cd8b941fc3f3d8451c3304f910874290
312080367 <asaped96@gmail.com>
2019-01-14 17:17:26 -0600

    Se hizo el ejercicio Diana

---
+++ Clases/Clases/Programas/Diana.py
@@ -0,0 +1,11 @@
+def diana(x,y):
+    if (x<5 or x>25) and y>30:
+        return (3)
+    if (x<5 or x>25) and y>10: 
+        return (5)
+    if (x<5 or x>25) and y<30:
+        return (5)
+    if (x<5 or x>25) and y<10:
+        return (3)
+    
+    
---
+++ Clases/Clases/Programas/función01.py
@@ -0,0 +1,10 @@
+def vAbsoluto(x):
+    if x >=0:
+        return(x)
+    else:
+        return(-x)
+
+while x>0:
+    x=x-1
+
+print x
